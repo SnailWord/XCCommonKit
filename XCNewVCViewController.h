57aa86b733cfbe8229bfe68fa97e95b55c0cc2bc
Yyy <yangyangy0423@126.com>
2020-03-06 17:30:24 +0800

    #Feature  new

---
+++ XCNewVCViewController.h
@@ -0,0 +1,16 @@
+//
+//  XCNewVCViewController.h
+//  Pods-XCCommonKit_Example
+//
+//  Created by James on 2020/3/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCNewVCViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
