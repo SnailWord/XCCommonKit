6c75988c3d234be481a879f3c5eaa74db70c0967
Yyy <yangyangy0423@126.com>
2020-03-06 16:50:07 +0800

    FirstSubmit

---
+++ Example/Classes/XCCommonTools.h
@@ -0,0 +1,17 @@
+//
+//  XCCommonTools.h
+//  XCCommonKit_Example
+//
+//  Created by James on 2020/3/6.
+//  Copyright © 2020 iTwinkle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCCommonTools : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/Classes/XCCommonTools.m
@@ -0,0 +1,13 @@
+//
+//  XCCommonTools.m
+//  XCCommonKit_Example
+//
+//  Created by James on 2020/3/6.
+//  Copyright © 2020 iTwinkle. All rights reserved.
+//
+
+#import "XCCommonTools.h"
+
+@implementation XCCommonTools
+
+@end
