e94e00ec9064ccac505866172d4e9f4747541ec7
Yyy <yangyangy0423@126.com>
2020-03-06 17:19:38 +0800

    结构

---
+++ XCCommonKit/Classes/XCQQ.h
@@ -0,0 +1,16 @@
+//
+//  XCQQ.h
+//  Pods-XCCommonKit_Example
+//
+//  Created by James on 2020/3/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCQQ : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCCommonKit/Classes/XCQQ.m
@@ -0,0 +1,12 @@
+//
+//  XCQQ.m
+//  Pods-XCCommonKit_Example
+//
+//  Created by James on 2020/3/6.
+//
+
+#import "XCQQ.h"
+
+@implementation XCQQ
+
+@end
