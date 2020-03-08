7dd817943c72869226f196121a44e5f04e772144
Yyy <yangyangy0423@126.com>
2020-03-08 13:12:15 +0800

    #Feature  update

---
+++ XCCommonKit/Assets/TwstTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  TwstTableViewCell.h
+//  AFNetworking
+//
+//  Created by James on 2020/3/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TwstTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCCommonKit/Assets/TwstTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TwstTableViewCell.m
+//  AFNetworking
+//
+//  Created by James on 2020/3/6.
+//
+
+#import "TwstTableViewCell.h"
+
+@implementation TwstTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
